ee7219e48bfa80a3f43cd1d6e60dfd578d6b0b51
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2021-12-14 16:13:27 +0700

    tweaked up

--- dist/cssfn.js
+++ dist/cssfn.js
@@ -20,11 +20,11 @@ const fastHash = (input) => {
         hash |= 0; // Convert to 32bit integer
     } // for
     hash = Math.abs(hash);
-    return hash.toString(36).slice(-4); // get the last 4 characters
+    return hash.toString(36).slice(-5); // get the last 5 characters
 };
 // jss:
 const createGenerateId = (options = {}) => {
-    const takenIds = new Set();
+    const takenHashes = new Set();
     return (rule, sheet) => {
         const globalID = (() => {
             let sheetId = sheet?.options?.sheetId ?? sheet?.options?.index ?? '';
@@ -37,18 +37,19 @@ const createGenerateId = (options = {}) => {
                 } // if
             } // if
             const classId = rule?.key || '@global';
-            let compoundId = `${sheetId}-${classId}`; // try to generate an unique Id _without_ a counter
+            const compoundId = `${sheetId}${classId}`; // try to generate an unique Id _without_ a counter
+            let hash = fastHash(compoundId);
             const maxCounter = 1e10;
             let counter = 2;
             for (; counter <= maxCounter; counter++) {
-                if (!takenIds.has(compoundId)) {
-                    takenIds.add(compoundId);
-                    return compoundId;
+                if (!takenHashes.has(hash)) {
+                    takenHashes.add(hash);
+                    return hash;
                 } // if
-                compoundId = `${sheetId}-${classId}-${counter}`; // try to generate an unique Id _with_ a counter
+                hash = fastHash(`${compoundId}${counter}`); // try to generate an unique Id _with_ a counter
             } // for
             warning(false, `[JSS] You might have a memory leak. ID counter is at ${counter}.`);
-            return compoundId;
+            return hash;
         })();
         const prefix = sheet?.options?.classNamePrefix ?? 'c';
         return `${prefix}${globalID}`;
